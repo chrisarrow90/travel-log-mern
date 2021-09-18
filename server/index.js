77df540cd8cc45bdff6d5ea0ef0cf1749487929d
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-09-18 23:49:35 +1200

    testing cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(
   cors({
-    origin: ['https://travel-log-mern.netlify.app/'],
+    origin: ['http://localhost:3000'],
     optionsSuccessStatus: 200
   })
 )
