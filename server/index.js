7081d1897d2636882b687c725b9afc8199e3c15c
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-09-18 23:47:01 +1200

    adjust cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const app = express()
 // Middleware
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors())
+app.use(
+  cors({
+    origin: ['https://travel-log-mern.netlify.app/']
+  })
+)
 
 // Routes
 app.use('/api/v1/posts', postRoutes)
