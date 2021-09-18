5fc914aa8ed8b6ef31fa41a0ffd1ad299fe6b22e
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-09-18 21:51:07 +1200

    add like functionality

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -5,7 +5,7 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 
-import { deletePost } from '../../../redux/actions/posts'
+import { deletePost, likePost } from '../../../redux/actions/posts'
 
 import useStyles from './styles'
 
@@ -33,15 +33,14 @@ const Post = ({ post, setCurrentId }) => {
         {post.title}
       </Typography>
       <CardContent>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="h6" gutterBottom>
           {post.message}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
           <ThumbUpAltIcon fontSize="small" />
-          Like
-          {post.likeCount}
+          {post.likeCount} Likes
         </Button>
         <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize="small" />
--- client/src/redux/actions/posts.js
+++ client/src/redux/actions/posts.js
@@ -36,3 +36,13 @@ export const deletePost = (id) => async (dispatch) => {
     console.error(err)
   }
 }
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id)
+
+    dispatch({ type: 'LIKE_POST', payload: data })
+  } catch (err) {
+    console.error(err)
+  }
+}
